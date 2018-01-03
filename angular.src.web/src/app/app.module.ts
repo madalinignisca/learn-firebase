224424c0e1d381b08f82816fd489555e0d59427c
Madalin Ignisca <madalin.ignisca@gmail.com>
2018-01-03 16:51:03 +0100

    Booom

--- angular.src.web/src/app/app.module.ts
+++ angular.src.web/src/app/app.module.ts
@@ -10,16 +10,29 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 
+import {
+  MdcButtonModule,
+  MdcToolbarModule
+} from '@angular-mdc/web';
+
+
+import { AuthComponent } from './auth/auth.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
+    MdcButtonModule,
+    MdcToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular.src.web/src/app/auth/auth.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+  constructor(public afAuth: AngularFireAuth) {
+  }
+
+  loginGoogle() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+}
